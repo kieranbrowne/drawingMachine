75a510da6def477f53d1f9b4948aac0955dbc17a
kieranbrowne <kierantbrowne@gmail.com>
2015-04-18 23:32:22 +1000

    ./resetArduino has full rebuild option

--- hardware/resetArduino.sh
+++ hardware/resetArduino.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-ln -s arduino/stdFirmataPlusServo/stdFirmataPlusServo.ino arduino/stdFirmataPlusServo/src/sketch.ino 
 clear
+if [ ${1:-foo} = "full" ]; then
+    echo ERASING AND REBUILDING INO
+    rm -r arduino/stdFirmataPlusServo/ino 
+    mkdir arduino/stdFirmataPlusServo/ino 
+    cd  arduino/stdFirmataPlusServo/ino
+    ino init
+    cd ../../../
+fi
+cp arduino/stdFirmataPlusServo/stdFirmataPlusServo.ino arduino/stdFirmataPlusServo/ino/src/sketch.ino 
+
 address=$(cat machine.config | grep serialAddress | sed 's/\s\s*/ /g' | cut -d' ' -f3)
 echo ATTEMPTING CONNECTION WITH PORT $address
 sleep 1
 
-cd arduino/stdFirmataPlusServo
+cd arduino/stdFirmataPlusServo/ino
 echo BUILDING FIRMATA
-ino build 
+ino build -m mega2560
 
 echo UPLOADING FIRMATA
 ino upload -p $address
